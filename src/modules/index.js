bec966d3b1120103e3bb2edaf5de7ca4b52b2078
Dilsher Balouch <dilsherb@gmail.com>
2023-03-15 17:42:33 +0300

    linter errors resolved

--- src/modules/index.js
+++ src/modules/index.js
@@ -139,23 +139,23 @@ const initialize = (element) => {
 };
 
 // modal implimentation started
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('myBtn');
+const span = document.getElementsByClassName('close')[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = () => {
+  modal.style.display = 'block';
+};
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = () => {
+  modal.style.display = 'none';
+};
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
 // modal implimentation ended
 
 document.addEventListener('DOMContentLoaded', () => initialize(mainContent));
